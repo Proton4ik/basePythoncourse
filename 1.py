680300bd930979254739b7d42f0e9b56302871a4
Proton4ik <75755359+Proton4ik@users.noreply.github.com>
2020-12-28 14:15:05 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+my_list = [12345, "text1", 5678, "text2"]
+print("Исходный список: ", my_list)
+print("Типы данных соответственно: ", type(my_list[0]), type(my_list[1]), type(my_list[2]), type(my_list[3]))
+print("Конец программы")
